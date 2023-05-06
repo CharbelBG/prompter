e67cdc8158ae6998a28ba2eb649a334f6aa17bf1
charbelBG <charbel.boughazale@gmail.com>
2023-05-06 08:58:08 +0300

    updated user navigation

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -3,15 +3,32 @@
 import {React, useState, useEffect} from 'react';
 import {signIn, signOut, useSession, getProviders} from 'next-auth/react';
 import Link from 'next/link';
-import Image from 'next/image';
 
+import Image from 'next/image';
 import styles from '../styles/navigation.module.css';
+
+const isUserLoggedIn = true;
+
 
 export default function Navigation(){
 return(
 <nav className={styles.navigation}>
-    <Image src='/assets/images/logo.svg' alt='Proompt Logo' height={30} width={40} />
-    <p>PROOMPTER</p>
+    <div>
+        <Image src='/assets/images/logo.svg' alt='Proompt Logo' height={30} width={40} />
+        <p>PROOMPTER</p>
+    </div>
+     
+    {isUserLoggedIn ? 
+    <div className={styles.headerBtns}>
+    <Link href="/">
+        Create Post
+    </Link>
+    <button type='button' onClick={signOut}>Sign Out</button>
+    {/* we can load here the user image from google sign in */}
+    <Image src='/assets/images/logo.svg' alt='profile' height={30} width={40} className={styles.profile} />
+    </div> : <div className={styles.headerBtns}>
+            <button onClick={signIn}>Sign In</button>
+    </div> }
 </nav>
 )
 }
